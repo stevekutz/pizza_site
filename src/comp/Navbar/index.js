15506754f5d0542bef679a4b6f1501909181a99f
stevekutz <stkutz@gmail.com>
2021-03-29 20:53:25 -0500

    setting up Router

---
+++ src/comp/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Nav, NavLink} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>  
+            
+                <NavLink to = '/'> Pizza </NavLink> 
+            </Nav>
+        
+        </>
+    
+    )
+
+
+}
+
+export default Navbar;
